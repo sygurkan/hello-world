ea5d30303084a5f71290fd781a871798b67f71b9
sygurkanAryaHP <sygurkan@gmail.com>
2017-12-05 20:24:04 +0100

    second commit

--- HelloWorld/Class1.cs
+++ HelloWorld/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            //
-        }
     }
 }
